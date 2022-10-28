293c3d91312508735711883857d2da81d27ed2e3
Tchisom17 <53842051+Tchisom17@users.noreply.github.com>
2022-10-28 16:05:17 +0100

    my stage one task

--- cmd/server/router.go
+++ cmd/server/router.go
@@ -1,25 +1,14 @@
 package server
 
 import (
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"time"
 	"zuri-stage-one/internal/adapters/api"
+	"zuri-stage-one/internal/core/middlewares"
 )
 
 func SetupRouter(handler *api.HTTPHandler) *gin.Engine {
 	router := gin.Default()
-	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},
-		AllowMethods:     []string{"POST", "GET", "PUT", "PATCH", "DELETE"},
-		AllowHeaders:     []string{"*"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		// AllowOriginFunc: func(origin string) bool {
-		// 	return origin == "https://github.com"
-		// },
-		MaxAge: 12 * time.Hour,
-	}))
+	router.Use(middlewares.CORS())
 
 	r := router.Group("/api/v1")
 	{
