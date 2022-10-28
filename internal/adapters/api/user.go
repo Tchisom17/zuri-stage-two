c7a9fe6c4acb17b79266757979b0d54f02f22d0e
Tchisom17 <53842051+Tchisom17@users.noreply.github.com>
2022-10-28 15:29:51 +0100

    my stage one task

--- internal/adapters/api/user.go
+++ internal/adapters/api/user.go
@@ -2,15 +2,13 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
-	"zuri-stage-one/internal/core/helpers"
 )
 
 func (u *HTTPHandler) Get(c *gin.Context) {
 	userLists, err := u.UserService.Get()
 	if err != nil {
-		helpers.JSON(c, "internal server error", 500, nil, []string{"An internal server error"})
+		c.JSON(500, nil)
 		return
 	}
-	helpers.JSON(c, "", http.StatusOK, userLists, nil)
+	c.JSON(200, userLists)
 }
