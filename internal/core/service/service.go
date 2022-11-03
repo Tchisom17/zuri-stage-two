1649997043d4c03189dfaf0ebe8d6a7e11d2f2e7
Tchisom17 <53842051+Tchisom17@users.noreply.github.com>
2022-11-03 16:30:51 +0100

    Create method to implement operation service interface

--- internal/core/service/service.go
+++ internal/core/service/service.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"strings"
+	"zuri-stage-one/internal/core/helpers"
 	"zuri-stage-one/internal/core/models"
 	"zuri-stage-one/internal/ports"
 )
@@ -15,6 +17,47 @@ func NewUserService(userRepository ports.UserRepository) ports.UserService {
 	}
 }
 
+type operationService struct {
+	operationRepository ports.OperationRepository
+}
+
+func NewOperationService(operationRepository ports.OperationRepository) ports.OperationService {
+	return &operationService{
+		operationRepository: operationRepository,
+	}
+}
+
 func (u *userService) Get() (*models.User, error) {
 	return u.userRepository.Get()
+}
+
+func (o *operationService) Calculate(operation *models.Operation) (*models.Operation, error) {
+	result := operation.ReadString(operation.OperationType)
+	if operation.X == 0 && operation.Y == 0 {
+		operation.X = result[0]
+		operation.Y = result[len(result)-1]
+	}
+
+	if strings.Contains(operation.OperationType, "add") || strings.Contains(operation.OperationType, "plus") || strings.Contains(operation.OperationType, "sum") {
+		operation.OperationType = "addition"
+	} else if strings.Contains(operation.OperationType, "subtract") || strings.Contains(operation.OperationType, "minus") || strings.Contains(operation.OperationType, "take away") {
+		operation.OperationType = "subtraction"
+	} else if strings.Contains(operation.OperationType, "times") || strings.Contains(operation.OperationType, "multiply") || strings.Contains(operation.OperationType, "product") {
+		operation.OperationType = "multiplication"
+	}
+
+	_, err := helpers.OperationEnum(operation.OperationType)
+	if err != nil {
+		return nil, err
+	}
+
+	if operation.OperationType == "addition" {
+		operation.Result = operation.X + operation.Y
+	} else if operation.OperationType == "subtraction" {
+		operation.Result = operation.X - operation.Y
+	} else {
+		operation.Result = operation.X * operation.Y
+	}
+
+	return o.operationRepository.Calculate(operation)
 }
