a14302eaf610e6196acee9ed9418bac1789bffee
Tchisom17 <53842051+Tchisom17@users.noreply.github.com>
2022-11-03 16:42:54 +0100

    code

--- internal/core/models/model.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type User struct {
-	SlackUsername string `json:"slackUsername"`
-	Backend       bool   `json:"backend"`
-	Age           int    `json:"age"`
-	Bio           string `json:"bio"`
-}
