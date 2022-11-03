b57f3eb7b42894e99aea0da9f07cbfd52bf70b8a
Tchisom17 <53842051+Tchisom17@users.noreply.github.com>
2022-11-03 16:31:41 +0100

    Create OperationRepository interface

--- internal/ports/repository.go
+++ internal/ports/repository.go
@@ -5,3 +5,7 @@ import "zuri-stage-one/internal/core/models"
 type UserRepository interface {
 	Get() (*models.User, error)
 }
+
+type OperationRepository interface {
+	Calculate(operation *models.Operation) (*models.Operation, error)
+}
