cb96d13cd225a4ebffe40db1722da195c689ddb4
Tchisom17 <53842051+Tchisom17@users.noreply.github.com>
2022-11-03 16:26:39 +0100

    Add the url path for Calculate method

--- cmd/server/router.go
+++ cmd/server/router.go
@@ -3,16 +3,17 @@ package server
 import (
 	"github.com/gin-gonic/gin"
 	"zuri-stage-one/internal/adapters/api"
-	"zuri-stage-one/internal/core/middlewares"
+	"zuri-stage-one/internal/core/middleware"
 )
 
 func SetupRouter(handler *api.HTTPHandler) *gin.Engine {
 	router := gin.Default()
-	router.Use(middlewares.CORS())
+	router.Use(middleware.CORS())
 
 	r := router.Group("/api/v1")
 	{
 		r.GET("/", handler.Get)
+		r.POST("/calc", handler.Calculate)
 	}
 
 	return router
